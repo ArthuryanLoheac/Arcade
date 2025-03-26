177d7ad0715ba47e1a29d42e4124021fe0db8161
Arthuryan Lohéac <146721013+ArthuryanLoheac@users.noreply.github.com>
2025-03-26 14:00:29 +0100

    :sparkles: game loop (#13)
    * ✨ Implement core game loop methods
    
    * ✨ Add Game Loop of the core
    
    * 📝  Fix getEvent method return type in IDisplayModule

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -13,6 +13,73 @@ Core::Core::Core()
     _game = nullptr;
 }
 
+Core::StateCore Core::Core::update()
+{
+    float deltaTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::steady_clock::now() - lastFrameTime).count();
+
+    if (_game->update(deltaTime))
+        return StateCore::EXIT_TO_MENU;
+    lastFrameTime = std::chrono::steady_clock::now();
+    return StateCore::NONE;
+}
+
+bool Core::Core::handleEventLibs(const Event &event)
+{
+    switch (event.key)
+    {
+        case Key::KeyCode::KEY_P:
+            openGameLib(_gameLibs[++_gameIndex % _gameLibs.size()]);
+            break;
+        case Key::KeyCode::KEY_O:
+            if (_gameIndex == 0)
+                openGameLib(_gameLibs[_gameLibs.size() - 1]);
+            else
+                openGameLib(_gameLibs[--_gameIndex % _gameLibs.size()]);
+            break;
+        case Key::KeyCode::KEY_I:
+            openDisplayLib(_displayLibs[++_displayIndex % _displayLibs.size()]);
+            break;
+        case Key::KeyCode::KEY_U:
+            if (_displayIndex == 0)
+                openDisplayLib(_displayLibs[_displayLibs.size() - 1]);
+            else
+                openDisplayLib(_displayLibs[--_displayIndex % _displayLibs.size()]);
+            break;
+        default:
+            return false;
+    }
+    return true;
+}
+
+Core::StateCore Core::Core::events()
+{
+    Event event = _display->getEvent();
+
+    while (event.key != Key::KeyCode::NONE) {
+        if (event.key == Key::KeyCode::ECHAP)
+            return StateCore::EXIT_TO_MENU;
+        if (event.key == Key::KeyCode::FUNCTION_4)
+            return StateCore::EXIT;
+        if (handleEventLibs(event)) {
+            if (_game->event(event))
+                return StateCore::EXIT_TO_MENU;
+        }
+        Event event = _display->getEvent();
+    }
+    return StateCore::NONE;
+}
+
+void Core::Core::draw()
+{
+    _display->clear();
+    for (auto drawable : _game->getDrawables())
+        _display->draw(drawable);
+    for (auto sound : _game->getSound())
+        _display->handleSound(sound);
+    _display->display();
+}
+
 void Core::Core::openGameLib(const std::string &gameLibPath)
 {
     if (_gameHandle)
